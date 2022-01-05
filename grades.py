04ba50e9d1dc52492d5b5add03ada8e1cbbd2c17
callmejazz <jmeade4347@gmail.com>
2022-01-05 13:35:24 -0800

    Grading Challenge resubmit

---
+++ grades.py
@@ -0,0 +1,11 @@
+your_grade = int(input("What kind of percentage did you get in programming this semester?\n"))
+if your_grade >= 90:
+  print("You got an A, great work!")
+elif your_grade >= 80:
+  print("You got an B, that's pretty good")
+elif your_grade >= 70:
+    print("You got an C, meh that's decent")
+elif your_grade >= 60:
+    print("You got an D, you're on the struggle bus here")
+else:
+  print("You got an F, tragic")
